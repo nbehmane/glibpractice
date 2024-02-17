83f28c084ef52c4721f1879992a5f4a62bc32713
Nima Behmanesh <nbehmane@calpoly.edu>
2024-02-16 19:56:11 -0600

    Created a simple adder.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PACKAGES= gtk+-3.0
+PACKAGES= gio-2.0 gio-unix-2.0 glib-2.0
 HDR=*.h
 SRC=*.c
 OBJ=*.o
@@ -7,6 +7,15 @@ PKG_CFLAGS= $(shell pkg-config --cflags $(PACKAGES))
 CPPFLAGS=-Wall -pedantic $(OPTIMFLAGS) $(PKG_CFLAGS)
 LDFLAGS= $(shell pkg-config --libs $(PACKAGES))
 BIN=out
+
+gen:
+	@gdbus-codegen --generate-c-code objecttest --interface-prefix ti.example. ti.example.ObjectTest.xml
+
+call:
+	gdbus call -e -d ti.example -o /ti/example/ObjectTest -m ti.example.ObjectTest.HelloWorld nima
+	gdbus call -e -d ti.example -o /ti/example/ObjectTest -m ti.example.ObjectTest.setA 2
+	gdbus call -e -d ti.example -o /ti/example/ObjectTest -m ti.example.ObjectTest.setB 2
+	gdbus call -e -d ti.example -o /ti/example/ObjectTest -m ti.example.ObjectTest.Add
 
 all: $(BIN)
 
--- main.c
+++ main.c
@@ -1,64 +1,116 @@
 #include <stdlib.h>
+#include <stdint.h>
 #include <gio/gio.h>
-#include "Object.h"
+#include "objecttest.h"
 
-static gboolean emit_object_cb(gpointer skeleton)
-{
-	object_emit_beep(OBJECT(skeleton));
-	object_set_activated(OBJECT(skeleton), FALSE);
-	return FALSE;
-}
+guint a = 0;
+guint b = 0;
 
-static gboolean on_handle_configure(Object *skeleton,
+static gboolean on_handle_add(ObjectTest *interface, 
 		GDBusMethodInvocation *invocation,
-		guint seconds,
+		guint output,
 		gpointer user_data)
 {
-	if (object_get_activated(skeleton)) 
-	{
-		g_dbus_method_invocation_return_error_literal(
-				invocation, G_IO_ERROR, G_IO_ERROR_EXISTS, "Exists");
-		return TRUE;
-	}
-
-	object_set_activated(skeleton, TRUE);
-	g_timeout_add_seconds(seconds, emit_object_cb, skeleton);
+	gchar *response = NULL;
+	response = g_strdup_printf("Output = %d.", a + b);
+	object_test_complete_set_a(interface, invocation, output);
+	g_print("%s\n", response);
+	g_free(response);
+	return TRUE;
 }
 
-static void on_name_acquired(GDBusConnection *connection,
-		const gchar *name,
+static gboolean on_handle_set_b(ObjectTest *interface, 
+		GDBusMethodInvocation *invocation,
+		guint received_b,
 		gpointer user_data)
 {
-	Object *skeleton;
-	skeleton = object_skeleton_new();
+	gchar *response = NULL;
+	response = g_strdup_printf("Set b = %d.", received_b);
+	object_test_complete_set_a(interface, invocation, received_b);
+	b = received_b;
+	g_print("%s\n", response);
+	g_free(response);
+	return TRUE;
+}
 
-	g_signal_connect(skeleton,
-			"handle-configure",
-			G_CALLBACK (on_handle_configure),
+static gboolean on_handle_set_a(ObjectTest *interface, 
+		GDBusMethodInvocation *invocation,
+		guint received_a,
+		gpointer user_data)
+{
+	gchar *response = NULL;
+	response = g_strdup_printf("Set a = %d.", received_a);
+	object_test_complete_set_a(interface, invocation, received_a);
+	a = received_a;
+	g_print("%s\n", response);
+	g_free(response);
+	return TRUE;
+}
+
+static gboolean on_handle_hello_world(ObjectTest *interface, 
+		GDBusMethodInvocation *invocation,
+		const char *greeting,
+		gpointer user_data)
+{
+	gchar *response = NULL;
+	response = g_strdup_printf("Hello world %s!!", greeting);
+	object_test_complete_hello_world(interface, invocation, response);
+	g_print("%s\n", response);
+	g_free(response);
+	return TRUE;
+}
+
+static void on_name_acquired(GDBusConnection *connection, 
+		const gchar *name, 
+		gpointer user_data)
+{
+	ObjectTest *interface = NULL;
+	GError *error = NULL;
+
+	interface = object_test_skeleton_new();
+	g_signal_connect(interface,
+			"handle-hello-world",
+			G_CALLBACK (on_handle_hello_world),
 			NULL);
 
-	g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON(skeleton),
-			connection,
-			"/ti/example/Object",
+	g_signal_connect(interface,
+			"handle-set-a",
+			G_CALLBACK (on_handle_set_a),
 			NULL);
+
+	g_signal_connect(interface,
+			"handle-set-b",
+			G_CALLBACK (on_handle_set_b),
+			NULL);
+
+	g_signal_connect(interface,
+			"handle-add",
+			G_CALLBACK (on_handle_add),
+			NULL);
+
+	g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON (interface), 
+			connection, 
+			"/ti/example/ObjectTest",
+			&error);
 }
 
 
 int main(void)
 {
 	g_print("---- Example Application ----\n");
-
 	GMainLoop *loop;
+
 	loop = g_main_loop_new(NULL, FALSE);
+
 	g_bus_own_name(G_BUS_TYPE_SESSION,
-			"ti.example.Object",
+			"ti.example",
 			G_BUS_NAME_OWNER_FLAGS_NONE,
 			NULL,
 			on_name_acquired,
 			NULL,
 			NULL,
 			NULL);
-
 	g_main_loop_run(loop);
+
 	return 0;
 }
